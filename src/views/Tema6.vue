00882a02beba333670a023c9ed09a59a4a1d4dc0
Luis Urueta <lgurueta@misena.edu.co>
2021-07-17 08:43:00 -0500

    Avance2

--- src/views/Tema6.vue
+++ src/views/Tema6.vue
@@ -3,6 +3,35 @@
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
+    .titulo-principal
+      .titulo-principal__numero
+        span 6
+      h1 Política de tratamiento de la información personal
+
+    p.mb-4 La política de tratamiento de datos personales es la que informa a los clientes de un establecimiento, por qué y el para qué se recolectan datos personales. Esta política debe estar expuesta en sitio público y ser divulgada por medios electrónicos como la página web y los correos electrónicos.
+
+    .row.justify-content-center.mb-4
+      .col-lg-10
+        .row.bg02.rounded
+          .col-md-7.p-4
+            p.mb-4 En Colombia, la protección de datos personales está reglamentada por la Ley 1581 de 2012. Puede descargar el texto aquí:
+            a.anexo.mb-4(href="https://www.funcionpublica.gov.co/eva/gestornormativo/norma.php?i=49981" target="_blank")
+              .anexo__icono
+                img(src="@/assets/template/icono-link.svg")
+              .anexo__texto
+                p Web. Función pública - Ley 1581 de 2012.
+            p.mb-4 La entidad rectora de la protección de datos personales es la Superintendencia de Industria y Comercio. En la página web de la entidad puede encontrar información importante respecto de los datos personales.
+            a.anexo(href="https://www.sic.gov.co/sobre-la-proteccion-de-datos-personales#:~:text=La%20Ley%20de%20Protecci%C3%B3n%20de,de%20naturaleza%20p%C3%BAblica%20o%20privada" target="_blank")
+              .anexo__icono
+                img(src="@/assets/template/icono-link.svg")
+              .anexo__text
+                p Web. Industria y Comercio - Protección de Datos Personales.
+          .col-md-5.position-relative
+            figure.position-absolute.bottom-0.end-0
+              img(src="@/assets/curso/cont6/img01.svg", alt="información personal") 
+
+    p Como recomendación, siempre se deben implementar las normas más estrictas en este sentido. Ya que el servicio de un hotel se presta no solo a nacionales, conviene que las normas de seguridad de información personal estén acordes con normas internacionales como las de la Comunidad Económica Europea.
+
 </template>
 
 <script>
--- src/views/Tema7.vue
+++ src/views/Tema7.vue
@@ -3,6 +3,61 @@
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
+    .titulo-principal
+        .titulo-principal__numero
+          span 7
+        h1 Reportes de reservas
+    
+    .row.justify-content-center.mb-5
+      .col-lg-6
+        figure
+          img(src="@/assets/curso/cont7/img01.svg", alt="información personal")
+
+    p.mb-4 Los sistemas PMS tienen una gran variedad de reportes; en la mayoría de ellos, incluso, se pueden personalizar reportes según la necesidad del establecimiento. 
+
+    p.mb-5 A continuación, se verá de forma gráfica una muestra de los reportes más utilizados en el área de reservas, tanto para reservas individuales como grupales.
+
+    .row.justify-content-center.mb-5
+      .col-lg-11
+        LineaTiempoD.color-primario
+          .row(numero="1" titulo="Lista de llegadas")
+            .col-12
+              p.mb-4 Lista todas las llegadas registradas, junto con los datos de ingreso y estado actual.
+
+              figure
+                img(src='@/assets/curso/cont7/02.png', alt='Lista de llegadas')
+
+          .row(numero="2" titulo="Gráfico de disponibilidad")
+            .col-12
+              p.mb-4 Lista todas las llegadas registradas, junto con los datos de ingreso y estado actual.
+
+              figure
+                img(src='@/assets/curso/cont7/03.png', alt='Gráfico de disponibilidad')
+          
+          .row(numero="3" titulo="Reporte de cancelaciones")
+            .col-12
+              p.mb-4 Lista todas las llegadas registradas, junto con los datos de ingreso y estado actual.
+
+              figure
+                img(src='@/assets/curso/cont7/04.png', alt='Reporte de cancelaciones')
+          
+          .row(numero="4" titulo="Reporte mensual de inventario")
+            .col-12
+              p.mb-4 Lista todas las llegadas registradas, junto con los datos de ingreso y estado actual.
+
+              figure
+                img(src='@/assets/curso/cont7/05.png', alt='Reporte mensual de inventario')
+          
+          .row(numero="5" titulo="Reporte de reservaciones")
+            .col-12
+              p.mb-4 Lista todas las llegadas registradas, junto con los datos de ingreso y estado actual.
+
+              figure
+                img(src='@/assets/curso/cont7/06.png', alt='Reporte de reservaciones')
+
+
+
+
 </template>
 
 <script>
